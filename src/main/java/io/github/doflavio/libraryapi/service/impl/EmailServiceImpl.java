73bdc262bd3a8af5766df75d3ae1bf043e83de9e
doflavio <doflavio@gmail.com>
2021-06-18 14:12:49 -0300

    Implmentando o serviço de email

--- src/main/java/io/github/doflavio/libraryapi/service/impl/EmailServiceImpl.java
+++ src/main/java/io/github/doflavio/libraryapi/service/impl/EmailServiceImpl.java
@@ -1,12 +1,34 @@
 package io.github.doflavio.libraryapi.service.impl;
 
 import io.github.doflavio.libraryapi.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
+
+    @Value("${}")
+    private String remetent;
+
+    private final JavaMailSender javaMailSender;
+
     @Override
     public void sendMails(String message, List<String> mailList) {
-        
+
+        String[] mails = mailList.toArray(new String[mailList.size()]);
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setFrom(remetent);
+        mailMessage.setSubject("Livro com empréstimo atrasado");
+        mailMessage.setText(message);
+        mailMessage.setTo(mails);
+
+        javaMailSender.send(mailMessage);
     }
 }
