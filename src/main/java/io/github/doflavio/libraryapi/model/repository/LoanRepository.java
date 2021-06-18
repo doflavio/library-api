c43cd530253cfcb2ca8c05aff65d53aa4aa540d0
FLAVIO LEANDRO PEREIRA <p060937@d02.prodemge.gov.br>
2021-06-18 12:43:34 -0300

    Realizando as alterações para receber o email do customer

--- src/main/java/io/github/doflavio/libraryapi/model/repository/LoanRepository.java
+++ src/main/java/io/github/doflavio/libraryapi/model/repository/LoanRepository.java
@@ -9,6 +9,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface LoanRepository extends JpaRepository<Loan,Long> {
 
     @Query(value = " select case when ( count (l.id) > 0) then true else false " +
@@ -22,4 +25,7 @@ public interface LoanRepository extends JpaRepository<Loan,Long> {
             , Pageable pageable);
 
     Page<Loan> findByBook(Book book, Pageable pageable);
+
+    @Query(" select l from Loan l where l.loanDate <= :threeDaysAgo and ( ")
+    List<Loan> findByLoanDateLessThanAndReturned(@Param("") LocalDate threeDaysAgo);
 }
