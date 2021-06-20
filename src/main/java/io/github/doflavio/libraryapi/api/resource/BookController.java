19c2767df1b09ce0eb02f4ebd0df7178120e6dea
doflavio <doflavio@gmail.com>
2021-06-20 09:17:06 -0300

    Ajustes erro ao publicar

--- src/main/java/io/github/doflavio/libraryapi/api/resource/BookController.java
+++ src/main/java/io/github/doflavio/libraryapi/api/resource/BookController.java
@@ -13,6 +13,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
+
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
